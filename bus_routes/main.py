4c2181c10c5c183025e74773b920b93938a2a4b7
Thomas Richards <thomas.c.richards@outlook.com>
2024-09-02 18:38:49 -0500

    Implement separate pickup/dropoff utilities, refactor

--- bus_routes/main.py
+++ bus_routes/main.py
@@ -1,15 +1,17 @@
 import os
 
+from copy import deepcopy
 import googlemaps
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, time
 from dotenv import load_dotenv
 from pathlib import Path
 
+
+print("-" * 55 + "\nGenerating Bus Route Directions!\n" + "-" * 55)
+
+
+# Retrieve API key from .env file (not checked into version control)
 load_dotenv()
-
-print("-"*55 + "\nGenerating Bus Route Directions!\n" + "-"*55)
-
-# Replace 'YOUR_API_KEY' with your actual Google Maps API key
 API_KEY = os.environ["GOOGLE_MAPS_API_KEY"]
 gmaps = googlemaps.Client(key=API_KEY)
 
@@ -21,65 +23,123 @@ addresses = [
     "914 40th Street, Brooklyn, NY 11219",
     "100 Caton Avenue, Brooklyn, NY 11218",
     "143 17th Street, Brooklyn, NY 11215",
-    "487 Carlton Avenue, Brooklyn, NY 11238", (AM only)
-    "1229 Atlantic Avenue, Brooklyn, NY 11216", (AM only)
+    "487 Carlton Avenue, Brooklyn, NY 11238",
+    "1229 Atlantic Avenue, Brooklyn, NY 11216",
     "789 St Marks Avenue, Brooklyn, NY 11213",
     "1430 Bergen Street, Brooklyn, NY 11213",
     "180 Troy Avenue, Brooklyn, NY 11213",
-    "1639 Carroll Street, Brooklyn, NY 11213"
+    "1639 Carroll Street, Brooklyn, NY 11213",
 ]
 
+addresses.reverse()
+
+
+def get_pickup_plan(addresses: list[str], required_at_school_by: datetime):
+    origin = addresses[0]
+    destination = addresses[-1]
+    waypoints = addresses[1:-1]
+    directions = get_directions(gmaps, origin, destination, waypoints)
+
+    pickup_times = []
+    total_duration = timedelta()
+    for leg in reversed(directions[0]["legs"]):
+        leg_duration = leg["duration"]["value"]
+        total_duration += timedelta(seconds=leg_duration)
+        pickup_times.append(required_at_school_by - total_duration)
+    pickup_times.reverse()
+
+    return directions, pickup_times
+
+
+def get_dropoff_plan(addresses: list[str], pickup_from_school_at: datetime):
+    addresses = deepcopy(addresses)
+    addresses.reverse()
+
+    origin = addresses[0]
+    destination = addresses[-1]
+    waypoints = addresses[1:-1]
+    directions = get_directions(gmaps, origin, destination, waypoints)
+
+    dropoff_times = []
+    total_duration = timedelta()
+    for leg in directions[0]["legs"]:
+        leg_duration = leg["duration"]["value"]
+        total_duration += timedelta(seconds=leg_duration)
+        dropoff_times.append(pickup_from_school_at + total_duration)
+
+    return directions, dropoff_times
 
 
 # Function to get directions
-def get_directions(gmaps, origin, destination, waypoints):
+def get_directions(gmaps, origin: str, destination: str, waypoints: list[str]):
     directions_result = gmaps.directions(
         origin,
         destination,
         mode="driving",
         waypoints=waypoints,
-        departure_time=datetime.now()
+        departure_time=datetime.now(),
     )
     return directions_result
 
-# Get the directions
-origin = addresses[0]
-destination = addresses[-1]
-waypoints = addresses[1:-1]
 
-directions = get_directions(gmaps, origin, destination, waypoints)
+def write_plan(directions: list[str], stop_times: list[time], file_name: str | None = None):
+    file_name = file_name or (
+        f"directions_{len(addresses)}_stops_from_{addresses[0]}_to_{addresses[-1]}"
+    )
+    out_path = Path(__file__).parent.parent / "output" / file_name
 
-# Calculate the recommended pickup times
-final_dropoff_time = datetime.strptime("08:00 AM", "%I:%M %p")
-pickup_times = []
+    # Save directions to a file
+    with open(out_path, "w") as f:
+        print()
+        for i, leg in enumerate(directions[0]["legs"]):
+            if i == 0:
+                f.write(
+                    f"Start at: {leg['start_address']} at {stop_times[i].strftime('%I:%M %p')}\n"
+                )
+            else:
+                f.write(
+                    f"{i}th Pick: {leg['start_address']} at {stop_times[i].strftime('%I:%M %p')}\n"
+                )
 
-total_duration = timedelta()
-for leg in reversed(directions[0]['legs']):
-    leg_duration = leg['duration']['value']
-    total_duration += timedelta(seconds=leg_duration)
-    pickup_times.append(final_dropoff_time - total_duration)
+            for step in leg["steps"]:
+                instructions = (
+                    step["html_instructions"]
+                    .replace('<div style="font-size:0.9em">', " (")
+                    .replace("</div>", ")")
+                    .replace("<b>", "")
+                    .replace("</b>", "")
+                    .replace("<br/>", "")
+                    .replace("/<wbr/>", "")
+                )
+                instructions = instructions.replace("&nbsp;", " ").replace("&amp;", "&")
+                distance = step["distance"]["text"]
+                duration = step["duration"]["text"]
+                f.write(f"{instructions} ({distance}, {duration})\n")
 
-pickup_times.reverse()
+            f.write("\n")
 
-out_name = f"directions_{len(addresses)}_stops_from_{addresses[0]}_to_{addresses[-1]}"
-out_path = Path(__file__).parent + out_name
+        f.write(
+            f"Arrive at: {directions[0]['legs'][-1]['end_address']} by {stop_times[-1]:%H:%M}\n"
+        )
 
-# Save directions to a file
-with open(out_path, "w") as f:
-    print()
-    for i, leg in enumerate(directions[0]['legs']):
-        if i == 0:
-            f.write(f"Start at: {leg['start_address']} at {pickup_times[i].strftime('%I:%M %p')}\n")
-        else:
-            f.write(f"{i}th Pick: {leg['start_address']} at {pickup_times[i].strftime('%I:%M %p')}\n")
-        
-        for step in leg['steps']:
-            instructions = step['html_instructions'].replace('<div style="font-size:0.9em">', ' (').replace('</div>', ')').replace('<b>', '').replace('</b>', '').replace('<br/>', '')
-            instructions = instructions.replace('&nbsp;', ' ').replace('&amp;', '&')
-            distance = step['distance']['text']
-            duration = step['duration']['text']
-            f.write(f"{instructions} ({distance}, {duration})\n")
-        
-        f.write("\n")
 
-    f.write(f"Arrive at: {directions[0]['legs'][-1]['end_address']} by 08:00 AM\n")
+# pickup_directions, pickup_times = get_pickup_plan(
+#     addresses, required_at_school_by=datetime(2024, 9, 1, 8)
+# )
+# 
+# write_plan(pickup_directions, pickup_times, "am_pickup.txt")
+
+pm_only_addresses = [
+    addr
+    for addr in addresses
+    if addr
+    not in {
+        "487 Carlton Avenue, Brooklyn, NY 11238",
+        "1229 Atlantic Avenue, Brooklyn, NY 11216",
+    }
+]
+dropoff_directions, dropoff_times = get_dropoff_plan(
+    pm_only_addresses, pickup_from_school_at=datetime(2024, 9, 1, 15)
+)
+
+write_plan(dropoff_directions, dropoff_times, "pm_dropoff.txt")
